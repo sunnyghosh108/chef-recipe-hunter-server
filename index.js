f673d16b2701059f76f82906ab828cd36f1351e5
sunnyghosh108 <sunnyghoshindra009@gmail.com>
2023-05-04 23:23:47 +0600

    correction

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const app=express();
 const cors = require('cors')
 const port=process.env.PORT || 5000;
 
-const categories =require('./data/category.json');
+const category =require('./data/category.json');
 
 app.use(cors());
 
 app.get('/category',(req,res)=>{
-    console.log(categories);
-    res.send(categories);
+    console.log(category);
+    res.send(category);
 
 });
 
 app.get('/category/:id',(req,res)=>{
     const id =req.params.id;
-    const item=categories.find((pd)=>pd.id==id)
+    const item=category.find((pd)=>pd.id==id)
    res.send(item);
 });
 app.listen(port,()=>{
